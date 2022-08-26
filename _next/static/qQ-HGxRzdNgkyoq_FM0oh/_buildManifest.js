a5d84de62d8a5d80e3acd4128cb5fa5f958f1ea0
smasoudhosseini <smasoudhosseini@alum.sharif.edu>
2022-08-26 18:42:29 +0330

    second commit

---
+++ _next/static/qQ-HGxRzdNgkyoq_FM0oh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f34b7386287348db.css","static/chunks/393-6360642d29e1f60c.js","static/chunks/pages/index-aaae0b72444f7957.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
